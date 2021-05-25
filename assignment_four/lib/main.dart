6e20b74292154568f63077dbaab146145c7466e5
Adam Scott <ajscott1@utas.edu.au>
2021-05-25 16:43:28 +1000

    Firebase added

--- assignment_four/lib/main.dart
+++ assignment_four/lib/main.dart
@@ -1,32 +1,66 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'ClassView.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tutor Marks',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Tutor Marks'),
-    );
-  }
+    return FutureBuilder(
+
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            //return FullScreenText(text:"Something went wrong");
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done)
+          {
+            return ChangeNotifierProvider(
+                create: (context) => null,
+                child: MaterialApp(
+                  title: 'Tutor Marks',
+                  theme: ThemeData(
+                  // This is the theme of the application.
+                    primarySwatch: Colors.blue,
+                  ),
+                 home: MyHomePage(title: 'Tutor Marks'),
+                )
+              );
+          }
+
+          return MaterialApp(
+            home:  Scaffold(
+              body: SafeArea(
+                child: Align(
+                    alignment: Alignment(0.5, 0.5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Loading Application', style: TextStyle(fontSize: 40),
+                      ),
+                    ]
+                  )
+                )
+              )
+            )
+          );
+        }
+      );
+    }
 }
 
 class MyHomePage extends StatefulWidget {
